03fe899559cf40320dfa07ab695445c394dda2cd
RC-14 <61058098+RC-14@users.noreply.github.com>
2024-05-03 23:03:14 +0200

    Create prefs.js

---
+++ prefs.js
@@ -0,0 +1,6 @@
+// The about:config options I set that aren't accessibla through any other GUI
+// Not actually a prefs.js file
+
+user_pref("browser.fullscreen.exit_on_escape", false);
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("xpinstall.signatures.required", false);
